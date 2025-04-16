3c0c5f1faccbf920f1e8705664f3b1383278c241
Jean <63162857+masckmaster2007@users.noreply.github.com>
2025-04-16 14:37:06 +0200

    I'm the worst geode dev ever

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 using namespace geode::prelude;
 
+#include <Geode/binding/GJHttpResult.hpp>
 #include <Geode/modify/GJAccountManager.hpp>
 class $modify(GJAccountManager) {
 	void handleIt(bool _requestSentSuccessfully, std::string _response, std::string _tag, GJHttpType _httpType) {
