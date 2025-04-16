b5aeb955e4e8dc556da56d12b619b7f3a6a30a50
Jean <63162857+masckmaster2007@users.noreply.github.com>
2025-04-16 14:46:28 +0200

    ty M336
    https://github.com/M336G/AutoLeaderboard/blob/main/src/main.cpp#L65C39-L65C40

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@ using namespace geode::prelude;
 class $modify(GJAccountManager) {
 	void handleIt(bool _requestSentSuccessfully, std::string _response, std::string _tag, GJHttpType _httpType) {
 		switch(_httpType) {
-			case 0x36:
-			case 0x37:
+			case GJHttpType::GetAccountBackupURL:
+			case GJHttpType::GetAccountSyncURL:
 				_response = "https://gdbackup.141412.xyz";
 				break;
 			default:
